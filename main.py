fefc45e253dddb58baaccc9b79869742d165fc2d
sher1ock-jp <takaaaaadayo@gmail.com>
2024-08-10 15:27:26 +0900

    add visualization(graphml outoput etc)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # Path to your .parquet file
-file_path = 'output/20240809-104350/artifacts/create_final_relationships.parquet'
+file_path = 'output/20240810-150058/artifacts/create_final_nodes.parquet'
 
-# Read the .parquet file
-df = pd.read_parquet(file_path)
+# df = pd.read_parquet(file_path)
+# print(df)
 
-# Display the DataFrame
-print(df)
+# G = nx.read_graphml("output/20240810-150058/artifacts/embedded_graph.0.graphml")
+G = nx.read_graphml("output/20240810-150058/artifacts/clustered_graph.1.graphml")
+# G = nx.read_graphml("output/20240810-150058/artifacts/merged_graph.graphml")
+
+nx.draw(G, with_labels=True)
+plt.show()
