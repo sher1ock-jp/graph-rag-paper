51cef8a1a43154457cd2afd37fa6c33a624403b7
sher1ock-jp <takaaaaadayo@gmail.com>
2024-08-11 14:54:40 +0900

    first commit

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # Path to your .parquet file
-file_path = 'output/20240810-150058/artifacts/create_final_nodes.parquet'
+file_path = 'output/20240811-142618/artifacts/create_final_relationships.parquet'
 
-# df = pd.read_parquet(file_path)
-# print(df)
+df = pd.read_parquet(file_path)
+print(df)
 
 # G = nx.read_graphml("output/20240810-150058/artifacts/embedded_graph.0.graphml")
-G = nx.read_graphml("output/20240810-150058/artifacts/clustered_graph.1.graphml")
+# G = nx.read_graphml("output/20240811-142618/artifacts/merged_graph.graphml")
 # G = nx.read_graphml("output/20240810-150058/artifacts/merged_graph.graphml")
 
-nx.draw(G, with_labels=True)
-plt.show()
+# nx.draw(G, with_labels=True)
+# plt.show()
